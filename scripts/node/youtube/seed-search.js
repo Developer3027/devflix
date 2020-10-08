6fc58dcc8664f3f8cd90e7a48b39579aa09b2689
apena415 <16394931+apena415@users.noreply.github.com>
2020-10-08 16:24:19 -0700

    testing

--- scripts/node/youtube/seed-search.js
+++ scripts/node/youtube/seed-search.js
@@ -76,9 +76,33 @@ const testDb = (db, collectionName) => {
   return db.collection(collectionName)
 }
 
+/*
+var citiesRef = db.collection("cities");
+
+citiesRef.doc("SF").set({
+  name: "San Francisco", state: "CA", country: "USA",
+  capital: false, population: 860000,
+  regions: ["west_coast", "norcal"] });
+citiesRef.doc("LA").set({
+  name: "Los Angeles", state: "CA", country: "USA",
+  capital: false, population: 3900000,
+  regions: ["west_coast", "socal"] });
+citiesRef.doc("DC").set({
+  name: "Washington, D.C.", state: null, country: "USA",
+  capital: true, population: 680000,
+  regions: ["east_coast"] });
+citiesRef.doc("TOK").set({
+  name: "Tokyo", state: null, country: "Japan",
+  capital: true, population: 9000000,
+  regions: ["kanto", "honshu"] });
+citiesRef.doc("BJ").set({
+  name: "Beijing", state: null, country: "China",
+  capital: true, population: 21500000,
+  regions: ["jingjinji", "hebei"] });
+*/
 
 // works good
-let testName = 'test' //'films'
+let testName = 'cities' //'films'
 testDb(db, testName)
   .get()
   .then((snapshot) => {

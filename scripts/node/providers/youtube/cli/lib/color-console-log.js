8fc38fb124edb4793556584e311f7ccec15f2987
apena415 <16394931+apena415@users.noreply.github.com>
2020-10-21 17:18:45 -0700

    custom true colors for wrapped console.log

---
+++ scripts/node/providers/youtube/cli/lib/color-console-log.js
@@ -0,0 +1,16 @@
+const c = require('../../../../local-console-logger.js').colorizeConsoleLog
+
+const trueColor = {
+  pastelOne: {
+    rose: c('#FFAFFF'),
+    shalimar: c('#FFFFAF'),
+    cornflower: c('#AFD7FF'),
+    mint: c('#AFFFD7'),
+    pink: c('#FFAFD7'),
+    peach: c('#FFD7AF'),
+    salmon: c('#FFAFAF')
+  }
+}
+module.exports = {
+  trueColor
+}

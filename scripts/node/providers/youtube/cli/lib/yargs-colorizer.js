69201d042e0c8295e3aaa4dbc45b6c9136607432
apena415 <16394931+apena415@users.noreply.github.com>
2020-10-21 22:26:01 -0700

    refactor, pastelColor method

--- scripts/node/providers/youtube/cli/lib/yargs-colorizer.js
+++ scripts/node/providers/youtube/cli/lib/yargs-colorizer.js
@@ -14,45 +14,87 @@
  */
 c = require('chalk')
 
+const C = {
+  rose: '#FFAFFF',
+  cornflower: '#AFD7FF',
+  mint: '#AFFFD7',
+  peach: '#FFD7AF',
+  shalimar: '#FFFFAF',
+  pink: '#FFAFD7'
+}
+
+const Options = (yargs, hex) => {
+  yargs.updateStrings({ 'Options:': c.hex(hex)('Options:') })
+}
+
+const Commands = (yargs, hex) => {
+  yargs.updateStrings({ 'Commands:': c.hex(hex)('Commands:') })
+}
+
+const Examples = (yargs, hex) => {
+  yargs.updateStrings({ 'Examples:': c.hex(hex)('Examples:') })
+}
+
+const ShowVersion = (yargs, hex) => {
+  yargs.updateStrings({ 'Show version number': c.hex(hex)('Show version number') })
+}
+
+const ShowHelp = (yargs, hex) => {
+  yargs.updateStrings({ 'Show help': c.hex(hex)('Show help') })
+}
+
+const NotEnoughNonOptionArgs = (yargs, hex) => {
+  yargs.updateStrings({
+    "Not enough non-option arguments: got %s, need at least %s": {
+      "one": c.red("Not enough non-option arguments: got %s, need at least %s"),
+      "other": c.red("Not enough non-option arguments: got %s, need at least %s")
+    }
+  })
+}
+
+const TooManyNonOptionArgs = (yargs, hex) => {
+  yargs.updateStrings({
+    "Too many non-option arguments: got %s, maximum of %s": {
+      "one": c.red("Too many non-option arguments: got %s, maximum of %s"),
+      "other": c.red("Too many non-option arguments: got %s, maximum of %s")
+    }
+  })
+}
+
+const aliases = (yargs, hex) => {
+  yargs.updateStrings({ 'aliases:': c.hex(hex)('aliases:') })
+}
+
+const command = (yargs, hex) => {
+  yargs.updateStrings({ 'command': c.hex(hex)('command') })
+}
+
+const boolean = (yargs, hex) => {
+  yargs.updateStrings({ 'boolean': c.hex(hex)('boolean') })
+}
+
+const pastelColor = (yargs) => {
+  Options(yargs, C.rose)
+  Commands(yargs, C.rose)
+  aliases(yargs, C.pink)
+  command(yargs, C.peach)
+  boolean(yargs, C.shalimar)
+  ShowVersion(yargs, C.mint)
+  ShowHelp(yargs, C.mint)
+  NotEnoughNonOptionArgs(yargs)
+  TooManyNonOptionArgs(yargs)
+}
+
 module.exports = {
-  Options: (yargs, hex) => {
-    yargs.updateStrings({ 'Options:': c.hex(hex)('Options:') })
-  },
-  Commands: (yargs, hex) => {
-    yargs.updateStrings({ 'Commands:': c.hex(hex)('Commands:') })
-  },
-  Examples: (yargs, hex) => {
-    yargs.updateStrings({ 'Examples:': c.hex(hex)('Examples:') })
-  },
-  ShowVersion: (yargs, hex) => {
-    yargs.updateStrings({ 'Show version number': c.hex(hex)('Show version number') })
-  },
-  ShowHelp: (yargs, hex) => {
-    yargs.updateStrings({ 'Show help': c.hex(hex)('Show help') })
-  },
-  NotEnoughNonOptionArgs: (yargs, hex) => {
-    yargs.updateStrings({
-      "Not enough non-option arguments: got %s, need at least %s": {
-        "one": c.red("Not enough non-option arguments: got %s, need at least %s"),
-        "other": c.red("Not enough non-option arguments: got %s, need at least %s")
-      }
-    })
-  },
-  TooManyNonOptionArgs: (yargs, hex) => {
-    yargs.updateStrings({
-      "Too many non-option arguments: got %s, maximum of %s": {
-        "one": c.red("Too many non-option arguments: got %s, maximum of %s"),
-        "other": c.red("Too many non-option arguments: got %s, maximum of %s")
-      }
-    })
-  },
-  aliases: (yargs, hex) => {
-    yargs.updateStrings({ 'aliases:': c.hex(hex)('aliases:') })
-  },
-  command: (yargs, hex) => {
-    yargs.updateStrings({ 'command': c.hex(hex)('command') })
-  },
-  boolean: (yargs, hex) => {
-    yargs.updateStrings({ 'boolean': c.hex(hex)('boolean') })
-  },
+  pastelColor,
+  Options,
+  Commands,
+  Examples,
+  ShowVersion,
+  ShowHelp,
+  NotEnoughNonOptionArgs,
+  TooManyNonOptionArgs,
+  aliases,
+  command,
+  boolean
 }

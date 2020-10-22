04796a5db340809506c76086d7e4d70137cfdff2
apena415 <16394931+apena415@users.noreply.github.com>
2020-10-21 17:20:50 -0700

    went crazy with custom colors

--- scripts/node/providers/youtube/data/terms-tool.js
+++ scripts/node/providers/youtube/data/terms-tool.js
@@ -5,30 +5,55 @@
  * and title list files must have their contents delimited by line breaks.
  * Run the tool without a coomand or with the -h or --help option for help.
  * @requires local-console-logger.js
+ * @requires yargs-colorizer.js
+ * @requires chalk
  * @example 
- *  // From the command line
- *  <code>node ./terms-tool.js front-end-terms.txt front-end-titles.txt</code>
+ *  // From the command line, visualize term and title files together
+ *  <code>node ./terms-tool.js show front-end-terms.txt front-end-titles.txt</code>
  * @license MIT
  */
 const fs = require('fs')
-const promisify = require('util').promisify
 
 const readline = require('readline')
 
+const chalk = require('chalk')
+
 const logger = require('../../../local-console-logger.js').console
+const cl = require('../cli/lib/color-console-log').trueColor.pastelOne
+const colorizeYargs = require('../cli/lib/yargs-colorizer.js')
+
+const C = {
+  rose: '#FFAFFF',
+  cornflower: '#AFD7FF',
+  mint: '#AFFFD7',
+  peach: '#FFD7AF',
+  shalimar: '#FFFFAF',
+  pink: '#FFAFD7'
+}
+
+const c = {
+  rose: chalk.hex(C.rose),
+  cornflower: chalk.hex(C.cornflower),
+  mint: chalk.hex(C.mint),
+  peach: chalk.hex(C.peach),
+  shalimar: chalk.hex(C.shalimar),
+  pink: chalk.hex(C.pink)
+}
+
+const colorizeArray = (strings, color = 'cornflower') => strings.map( str => eval(`c.${color}(str)`) )
 
 const COMMAND_NAMES = ['show', 'check']
 
 const CMD_SHOW = {
   NAME: COMMAND_NAMES[0],
   ALIASES: ['s','view'],
-  DESC: 'Show terms and their corresponding titles side by side.'
+  DESC: c.mint('Show terms and their corresponding titles side by side.')
 }
 
 const CMD_CHECK = {
   NAME: COMMAND_NAMES[1],
   ALIASES: ['c', 'verify'],
-  DESC: 'Verifies that terms and title files are of equal length.'
+  DESC: c.mint('Verifies that terms and title files are of equal length.')
 }
 
 const parseLines = (uri) => {
@@ -94,16 +119,16 @@ const cmdShow = async(argv, p = program) => {
     terms.length =  titles.length
   }
 
-  logger.info(`Creating a visual combination of ${terms.length} terms and ${titles.length} titles from the two files...`)
+  cl.rose.log(`Creating a visual combination of ${terms.length} terms and ${titles.length} titles from the two files...`)
   console.log('')
 
   for (let i = 0; i < terms.length; i++) {
     terms[i]
-      ? logger.log(`Term: ${terms[i]}`) 
+      ? cl.cornflower.log(`Term: ${terms[i]}`) 
       : logger.error(`Term: ${terms[i]}`) 
 
     titles[i] 
-      ? logger.info(`Title: ${titles[i]}`)
+      ? cl.mint.log(`Title: ${titles[i]}`)
       : logger.error(`Title: ${titles[i]}`)
     
     console.log()
@@ -112,11 +137,13 @@ const cmdShow = async(argv, p = program) => {
 }
 
 const cmdCheck = (argv, p = program) => {
-  console.log(`Verifying length of ${argv.terms} with ${argv.titles}`)
+  cl.rose.log(`Verifying length of ${argv.terms} with ${argv.titles}`)
 }
 
 const program = require('yargs/yargs')(process.argv.slice(2))
-  .scriptName('terms-tool')
+  .scriptName(c.cornflower('terms-tool'))
+  .version(c.cornflower('terms-tool 0.1.1'))
+  .epilog(c.shalimar('\n© 2020 Devz3n.com'))
   .command({
     command: `${CMD_SHOW.NAME} <terms> <titles>`,
     aliases: CMD_SHOW.ALIASES,
@@ -130,7 +157,7 @@ const program = require('yargs/yargs')(process.argv.slice(2))
     handler: cmdCheck
   })
   .wrap(72)
-  .demandCommand(1, 1, 'Error: You must use exactly one command with the proper number of required arguments.')
+  .demandCommand(3, 3, chalk.red('Error: You must use exactly one command with the proper number of required arguments.'))
   .help();
 
 const main = async(p = program) => {
@@ -143,6 +170,15 @@ const main = async(p = program) => {
 }
 
 // BEGIN: Main Program
+colorizeYargs.Options(program, C.rose)
+colorizeYargs.Commands(program, C.rose)
+colorizeYargs.aliases(program, C.pink)
+colorizeYargs.command(program, C.peach)
+colorizeYargs.boolean(program, C.shalimar)
+colorizeYargs.ShowVersion(program, C.mint)
+colorizeYargs.ShowHelp(program, C.mint)
+colorizeYargs.NotEnoughNonOptionArgs(program)
+colorizeYargs.TooManyNonOptionArgs(program)
 main(program)
 // END: Main Program
 
